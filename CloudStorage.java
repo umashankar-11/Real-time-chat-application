8028c5861874b5cf72789cd1c9b58740d5dfe7f7
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-25 21:22:12 +0530

    Update CloudStorage.java

--- CloudStorage.java
+++ CloudStorage.java
@@ -4,18 +4,20 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 
-public class CloudStorage {
+public class CloudStorage 
+{
     private static final AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
-    private static final String BUCKET_NAME = "your-bucket-name"; // Replace with your actual bucket name
+    private static final String BUCKET_NAME = "your-bucket-name"; 
 
-    // Upload file to Amazon S3
-    public static void uploadFile(File file) {
-        s3Client.putObject(new PutObjectRequest(BUCKET_NAME, file.getName(), file)); // Upload the file to S3
+    public static void uploadFile(File file) 
+    {
+        s3Client.putObject(new PutObjectRequest(BUCKET_NAME, file.getName(), file));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         File file = new File("path/to/your/file.txt");
-        uploadFile(file); // Upload the file
+        uploadFile(file);
         System.out.println("File uploaded successfully!");
     }
 }
