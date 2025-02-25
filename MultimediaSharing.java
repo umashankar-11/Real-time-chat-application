89f19fdb3ae0b5ea57b1f2fbf150a97303b4b74e
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-25 22:06:26 +0530

    Update MultimediaSharing.java

--- MultimediaSharing.java
+++ MultimediaSharing.java
@@ -3,15 +3,18 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class MultimediaSharing {
-    // Upload a file from local storage to a destination path
-    public static void uploadFile(File file, String destinationPath) throws IOException {
+public class MultimediaSharing 
+{   
+    public static void uploadFile(File file, String destinationPath) throws IOException
+    {
         try (FileInputStream inputStream = new FileInputStream(file);
-             FileOutputStream outputStream = new FileOutputStream(destinationPath)) {
-            byte[] buffer = new byte[1024]; // Buffer to read the file in chunks
+             FileOutputStream outputStream = new FileOutputStream(destinationPath)) 
+        {
+            byte[] buffer = new byte[1024]; 
             int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length); // Write data to the destination
+            while ((length = inputStream.read(buffer)) > 0) 
+            {
+                outputStream.write(buffer, 0, length); 
             }
         }
     }
