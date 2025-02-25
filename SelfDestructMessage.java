86144fc7fb016e26772c1dfc6e479b8a97e55685
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-25 22:25:43 +0530

    Update SelfDestructMessage.java

--- SelfDestructMessage.java
+++ SelfDestructMessage.java
@@ -1,29 +1,34 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SelfDestructMessage {
+public class SelfDestructMessage 
+{
     private String message;
     private Timer timer;
 
-    // Constructor to set self-destruct timer
-    public SelfDestructMessage(String message, int seconds) {
+    public SelfDestructMessage(String message, int seconds) 
+    {
         this.message = message;
         this.timer = new Timer();
-        timer.schedule(new TimerTask() {
+        timer.schedule(new TimerTask() 
+                       {
             @Override
-            public void run() {
-                message = null; // Self-destruct the message after the time expires
+            public void run()
+                           {
+                message = null;
                 System.out.println("Message has self-destructed.");
             }
-        }, seconds * 1000); // Set the time for self-destruction
+        }, seconds * 1000); 
     }
 
-    public String getMessage() {
+    public String getMessage() 
+    {
         return message;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SelfDestructMessage selfDestructMessage = new SelfDestructMessage("This is a secret!", 5);
-        System.out.println("Message: " + selfDestructMessage.getMessage()); // Output: This is a secret!
+        System.out.println("Message: " + selfDestructMessage.getMessage()); 
     }
 }
