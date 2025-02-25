3a6f7972610a2b12e181a27f40605e196a4f5da6
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-25 21:25:08 +0530

    Update EncryptionUtil.java

--- EncryptionUtil.java
+++ EncryptionUtil.java
@@ -2,34 +2,36 @@ import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-public class EncryptionUtil {
+public class EncryptionUtil 
+{
     private static final String ALGORITHM = "AES";
 
-    // Encrypt message using AES encryption algorithm
-    public static String encrypt(String data, String key) throws Exception {
+    public static String encrypt(String data, String key) throws Exception 
+    {
         SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] encrypted = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encrypted); // Returning encrypted data as base64 string
+        return Base64.getEncoder().encodeToString(encrypted); 
     }
-
-    // Decrypt message using AES decryption algorithm
-    public static String decrypt(String encryptedData, String key) throws Exception {
+    
+    public static String decrypt(String encryptedData, String key) throws Exception
+    {
         SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decodedData = Base64.getDecoder().decode(encryptedData); // Decode from base64
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
         byte[] decrypted = cipher.doFinal(decodedData);
-        return new String(decrypted); // Returning decrypted message as string
+        return new String(decrypted);
     }
 
-    public static void main(String[] args) throws Exception {
-        String key = "1234567890123456"; // AES key must be 16 bytes
+    public static void main(String[] args) throws Exception 
+    {
+        String key = "1234567890123456"; 
         String encryptedMessage = encrypt("Hello World!", key);
-        System.out.println("Encrypted: " + encryptedMessage); // Example output: Encrypted: Bl1vDd9m5ty=
+        System.out.println("Encrypted: " + encryptedMessage);
 
         String decryptedMessage = decrypt(encryptedMessage, key);
-        System.out.println("Decrypted: " + decryptedMessage); // Example output: Decrypted: Hello World!
+        System.out.println("Decrypted: " + decryptedMessage); 
     }
 }
