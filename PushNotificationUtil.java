2951cd145bdd998820007c6bd4af7b6302a565c8
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-25 22:22:59 +0530

    Update PushNotificationUtil.java

--- PushNotificationUtil.java
+++ PushNotificationUtil.java
@@ -1,16 +1,16 @@
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 
-public class PushNotificationUtil {
-    // Send push notification using Firebase Cloud Messaging
-    public static void sendPushNotification(String token, String title, String body) {
+public class PushNotificationUtil 
+{
+    public static void sendPushNotification(String token, String title, String body) 
+    {
         Message message = Message.builder()
-                .setToken(token) // The device token to send notification to
-                .putData("title", title) // Title of the notification
-                .putData("body", body) // Body content of the notification
+                .setToken(token) 
+                .putData("title", title) 
+                .putData("body", body) 
                 .build();
 
-        // Send notification asynchronously
         FirebaseMessaging.getInstance().sendAsync(message);
     }
 }
